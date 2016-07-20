defea7ea9894dcc9250ed7ba53e7801538e583a4
Tom Fuller <tofuller30@gmail.com>
2016-07-20 14:24:47 -0400

    Updated to not use Start-Process
    Updates to use the System.Diagnostics.Process object instead of Start-Process

--- scripts/Run-InitialBuild.ps1
+++ scripts/Run-InitialBuild.ps1
@@ -11,8 +11,22 @@ try {
     $ErrorActionPreference = "Stop"
 
     Start-Transcript -Path c:\cfn\log\Run-InitialBuild.ps1.txt -Append
-    
-    Start-Process "'"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'"" -ArgumentList '"$BuildFilePath" /p:BucketName=$BucketName /t:Build;Deploy' -Wait
+
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo.FileName = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+    $process.StartInfo.Arguments = "$BuildFilePath /p:BucketName=$BucketName /t:Build;Deploy"
+    $process.StartInfo.UseShellExecute = $false
+    $process.StartInfo.RedirectStandardOutput = $true
+
+    if($process.Start() ){
+       $output = $process.StandardOutput.ReadToEnd()
+       Write-Verbose $output
+    }
+    $process.WaitForExit()
+
+    <#
+       Start-Process "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" -ArgumentList "$BuildFilePath /p:BucketName=$BucketName /t:Build;Deploy" -Wait
+    #>
 }
 catch {
     Write-Verbose "$($_.exception.message)@ $(Get-Date)"
