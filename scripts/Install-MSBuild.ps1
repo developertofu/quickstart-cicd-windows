a4b0f7fe28a8b9b2d8b0db7aed23f41b09b03cb4
Tom Fuller <tofuller30@gmail.com>
2016-07-27 15:53:29 -0400

    Fixed log file location
    Moved it from C:\Projects to C:\cfn\log for consistency

--- scripts/Install-MSBuild.ps1
+++ scripts/Install-MSBuild.ps1
@@ -3,7 +3,7 @@ try {
 
     Start-Transcript -Path c:\cfn\log\Install-MSBuild.ps1.txt -Append
     
-    Start-Process "C:\cfn\downloads\BuildTools_Full.exe" -ArgumentList '/q /l C:\Projects\BuildTools-Install-Log.txt' -Wait
+    Start-Process "C:\cfn\downloads\BuildTools_Full.exe" -ArgumentList '/q /l C:\cfn\log\BuildTools-Install-Log.txt' -Wait
 }
 catch {
     Write-Verbose "$($_.exception.message)@ $(Get-Date)"
