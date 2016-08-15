6a63a2f9a7463ce4b1b8b896ca56f71a24df34a7
Tom Fuller <tofuller30@gmail.com>
2016-08-15 17:56:26 -0400

    bug fixes

--- scripts/Install-DotNetBuildServer-Jenkins.ps1
+++ scripts/Install-DotNetBuildServer-Jenkins.ps1
@@ -21,22 +21,40 @@ try {
     $oldjavaparms = $jenkinsConfig.service.arguments
     $jenkinsConfig.service.arguments = "-Djava.awt.headless=true -Dhudson.diyChunking=false " + $oldjavaparms
     $jenkinsConfig.Save("C:\Program Files (x86)\Jenkins\jenkins.xml")
-        
+    
+    $jenkinsCLIConfig = [xml](Get-Content "C:\Program Files (x86)\Jenkins\config.xml")
+    $jenkinsCLIConfig.hudson.slaveAgentPort = "0"
+    $jenkinsCLIConfig.Save("C:\Program Files (x86)\Jenkins\config.xml")
+    
     Copy-Item "C:\cfn\downloads\msbuild.hpi" "C:\Program Files (x86)\Jenkins\plugins"
     Copy-Item "C:\cfn\downloads\aws-codepipeline.hpi" "C:\Program Files (x86)\Jenkins\plugins"
     Copy-Item "C:\cfn\downloads\groovy.hpi" "C:\Program Files (x86)\Jenkins\plugins"
 
     Restart-Service "Jenkins"
 
+    $svc = Get-Service "Jenkins"
+    $timer = 0
+    while($svc.Status -ne 'Running')
+    {   
+       Start-Sleep -s 10
+       if($timer -le 300)
+       {
+          $timer = $timer + 10
+       }
+       else
+       {
+          throw "Jenkins service took longer than 5 minutes to reach running state."
+       }
+    }
+
     $adminpassword = Get-Content "C:\Program Files (x86)\Jenkins\secrets\initialAdminPassword"
     
     cd "C:\Program Files (x86)\Jenkins\jre\bin"
 
-    cmd /c 'java.exe -jar "C:\Program Files (x86)\Jenkins\war\WEB-INF\jenkins-cli.jar" -s http://localhost:8080 groovy --username admin --password $adminpassword = < c:\cfn\scripts\Create-Jenkins-User.groovy'
+    cmd /c 'java.exe -jar "C:\Program Files (x86)\Jenkins\war\WEB-INF\jenkins-cli.jar" -s http://localhost:8080 groovy --username admin --password $adminpassword = < c:\cfn\config\Create-Jenkins-User.groovy'
     #Remove-Item "C:\cfn\scripts\Create-Jenkins-User.groovy"
     
-    cmd /c 'java.exe -jar "C:\Program Files (x86)\Jenkins\war\WEB-INF\jenkins-cli.jar" -s http://localhost:8080 create-job $JobName --username admin --password $adminpassword < c:\cfn\config\config.xml'
-   
+    cmd /c 'java.exe -jar "C:\Program Files (x86)\Jenkins\war\WEB-INF\jenkins-cli.jar" -s http://localhost:8080 create-job $JobName --username admin --password $adminpassword < c:\cfn\config\config.xml'   
 }
 catch {
     Write-Verbose "$($_.exception.message)@ $(Get-Date)"
